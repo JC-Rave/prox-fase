93bf2e9815810eedb9d283e815c193556196d364
Jean Rave <jeanclon200021@gmail.com>
2024-01-14 23:54:57 -0500

    Se crea la clase Main y se configura conexion a internet

--- src/main.py
+++ src/main.py
@@ -1,4 +1,29 @@
+import network, time
 from proxsafe import ProxSafe
 
-prox_safe = ProxSafe()
-prox_safe.run()
+class Main:
+
+    def __init__(self, ssid, password):
+        self.__connect_to_network(ssid, password)
+        self.__prox_safe = ProxSafe()
+        self.__prox_safe.run()
+
+    def __connect_to_network(self, ssid, password):
+        self.__network = network.WLAN(network.STA_IF)
+
+        if not self.__network.isconnected():         #Si no está conectado…
+            self.__network.active(True)              #activa la interface
+            self.__network.connect(ssid, password)   #Intenta conectar con la red
+            print(f"Connecting to the '{ssid}' network...")
+
+            timeout = time.time()
+            while not self.__network.isconnected():  #Mientras no se conecte..
+                if (time.ticks_diff(time.time(), timeout) > 10):
+                    self.__network.active(False)
+                    raise Exception(f"Could not connect to '{ssid}' network")
+            else:
+                print(f"Connected to the '{ssid}' network")
+                print("Network data (IP/netmask/gw/DNS):", self.__network.ifconfig())
+
+
+Main("my_red", "my_password")
