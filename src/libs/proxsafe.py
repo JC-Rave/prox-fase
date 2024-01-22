8460ac61d45d1b50df63dac18d8dd5e3da33a44d
Jean Rave <jeanclon200021@gmail.com>
2024-01-21 23:00:53 -0500

    Se procesa y registran datos de proximidad en firebase

--- src/libs/proxsafe.py
+++ src/libs/proxsafe.py
@@ -1,7 +1,8 @@
 import ufirebase as firebase
 from machine import Pin, PWM
 from hcsr04 import HCSR04
-from utime import sleep
+from utime import sleep, time, localtime
+import _thread
 
 class ProxSafe:
 
@@ -11,28 +12,82 @@ class ProxSafe:
 
 
     def __init__(self):
-        self.__north_sensor = HCSR04(trigger_pin=27, echo_pin=26)
-        self.__south_sensor = HCSR04(trigger_pin=33, echo_pin=32)
-        self.__east_sensor = HCSR04(trigger_pin=5, echo_pin=17)
-        self.__west_sensor = HCSR04(trigger_pin=23, echo_pin=22)
-        self.__buzzer_sensor = PWM(Pin(4))
+        self.__front_sensor = HCSR04(trigger_pin=27, echo_pin=26)
+        self.__right_sensor = HCSR04(trigger_pin=33, echo_pin=32)
+        self.__rear_sensor = HCSR04(trigger_pin=5, echo_pin=17)
+        self.__left_sensor = HCSR04(trigger_pin=23, echo_pin=22)
+        self.__buzzer_sensor = PWM(Pin(21))
         self.__buzzer_sensor.duty(0)
 
 
     def run(self):
-        while True:
-            north_distance = self.__north_sensor.distance_cm()
-            south_distance = self.__south_sensor.distance_cm()
-            east_distance = self.__east_sensor.distance_cm()
-            west_distance = self.__west_sensor.distance_cm()
+        self.__proximity_data_list = []
+        _thread.start_new_thread(self.__insert_proximity_data, ())
 
-            if north_distance <= self.__MINIMAL_DISTANCE or south_distance <= self.__MINIMAL_DISTANCE or \
-            east_distance <= self.__MINIMAL_DISTANCE or west_distance <= self.__MINIMAL_DISTANCE:
+        while True:
+            front_distance = self.__front_sensor.distance_cm()
+            right_distance = self.__right_sensor.distance_cm()
+            rear_distance = self.__rear_sensor.distance_cm()
+            left_distance = self.__left_sensor.distance_cm()
+
+            if front_distance <= self.__MINIMAL_DISTANCE or right_distance <= self.__MINIMAL_DISTANCE or \
+            rear_distance <= self.__MINIMAL_DISTANCE or left_distance <= self.__MINIMAL_DISTANCE:
+                print("Object detected, activating alarm...")
                 self.__buzzer_sensor.freq(self.__FREQUENCY)
                 self.__buzzer_sensor.duty(self.__WORK_CYCLE)
-                firebase.put("Data", north_distance, bg=0)
+
+                print("Procesing data...")
+                datetime = self.__get_date_time()
+                data = {
+                    "date": datetime["date"],
+                    "time": datetime["time"],
+                    "crash_occurred": self.__is_crash_occurred(front_distance, right_distance, rear_distance, left_distance),
+                    "side": self.__get_occurrence_side(front_distance, right_distance, rear_distance, left_distance)
+                }
+
+                self.__proximity_data_list.append(data)
+                print(f"Processed data: {data}\n")
             else:
                 self.__buzzer_sensor.duty(0)
 
-            sleep(4)
+            sleep(1)
+
+
+    def __insert_proximity_data(self):
+        while True:
+            sleep(5)
+
+            if len(self.__proximity_data_list):
+                proximity_data_list = self.__proximity_data_list.copy()
+                self.__proximity_data_list.clear()
+
+                for data in proximity_data_list:
+                    print("Recording data...")
+                    firebase.put(f"proximity_data/{data["date"]} {data["time"]}", data, bg=0)
+                    print(f"Recorded data: {data}\n")
+
+
+    def __get_date_time(self):
+        now = localtime(time())
+        return {
+            "date": f"{now[0]}-{now[1]:02d}-{now[2]:02d}",
+            "time": f"{now[3]:02d}:{now[4]:02d}:{now[5]:02d}"
+        }
+
+
+    def __is_crash_occurred(self, front_distance, right_distance, rear_distance, left_distance):
+        return front_distance <= 2 or right_distance <= 2 or rear_distance <= 2 or left_distance <= 2
+
+
+    def __get_occurrence_side(self, front_distance, right_distance, rear_distance, left_distance):
+        if front_distance <= self.__MINIMAL_DISTANCE:
+            return "Delantero"
+        elif right_distance <= self.__MINIMAL_DISTANCE:
+            return "Derecha"
+        elif rear_distance <= self.__MINIMAL_DISTANCE:
+            return "Trasero"
+        elif left_distance <= self.__MINIMAL_DISTANCE:
+            return "Izquierda"
+        else:
+            return "Desconocido"
 
